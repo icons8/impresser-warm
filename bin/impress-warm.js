c551b30681a6c7566f9a2397373272ceb19cdef3
Viacheslav Bereza <betula.icons8@gmail.com>
2015-04-17 00:51:03 +0300

    Migrate to native libs

--- bin/impress-warm.js
+++ bin/impress-warm.js
@@ -8,13 +8,12 @@ var
 
   argv = yargs
     .usage('Usage: $0 [options]')
-    .describe('impress-host', 'Host of impress server by default localhost')
-    .describe('impress-port', 'Port of impress server by default 8497')
-    .describe('concurrent', 'Count of parallels requests by default 2 on each CPU cores')
+    .describe('impress-host', 'Host of impress server by default localhost:8497')
+    .describe('parallel', 'Count of parallels requests by default 2 on each CPU cores')
     .describe('request-timeout', 'Request timeout by default 61000')
     .describe('min-request-interval', 'Minimum request interval by default 1000')
-    .describe('sitemap-url', 'Url to sitemap xml file, can be multiple')
-    .describe('site-host', 'Host of target site')
+    .describe('sitemap', 'Url to sitemap xml file, can be multiple')
+    .describe('site-host', 'Host of target site by default icons8.com')
     .describe('config', 'Path to config file, can be multiple')
     .help('h')
     .alias('h', 'help')
